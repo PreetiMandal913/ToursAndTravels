7e9cc3dde69b9f2027066fa6f80b9ad2869afc8c
PreetiMandal913 <preetimandal913@gmail.com>
2024-07-20 21:21:56 +0530

    Initial commit

---
+++ index-js.js
@@ -0,0 +1,37 @@
+function opensignup()
+	{
+		document.getElementById("sign_up").style.backgroundColor="#00000095"
+		document.getElementById("sign_up").style.display="flex";
+	}
+	function closesignup()
+	{
+		document.getElementById("sign_up").style.display="none";
+	}
+	function openlogin()
+	{
+		document.getElementById("log_in").style.backgroundColor="#00000095"
+		document.getElementById("log_in").style.display="flex";
+	}
+	function closelogin()
+	{
+		document.getElementById("log_in").style.display="none";
+	}
+	function openloginas()
+	{
+		document.getElementById("log_as").style.backgroundColor="#00000095"
+		document.getElementById("log_as").style.display="flex"
+	}
+
+	function closeloginas()
+	{
+		document.getElementById("log_as").style.display="none"
+	}
+	function openadminlogin()
+	{
+		document.getElementById("admin_log_in").style.backgroundColor="#00000095"
+		document.getElementById("admin_log_in").style.display="flex"
+	}
+	function closeadminlogin()
+	{
+		document.getElementById("admin_log_in").style.display="none"
+	}
